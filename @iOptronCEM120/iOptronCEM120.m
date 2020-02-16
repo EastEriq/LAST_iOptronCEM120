24cc9b261a03834803871a556906d23772d7845d
Enrico Segre <enrico.segre@weizmann.ac.il>
2020-02-16 19:00:50 +0200

    first getters and setters for coordinates

--- @iOptronCEM120/iOptronCEM120.m
+++ @iOptronCEM120/iOptronCEM120.m
@@ -45,4 +45,64 @@ classdef iOptronCEM120 <handle
         
     end
     
+    methods
+        % setters and getters
+                function AZ=get.Az(I)
+            resp=I.query('GAC');
+            AZ=str2double(resp(10:18))/360000;
+        end
+        
+        function set.Az(I,AZ)
+            I.query(sprintf('Sz%09d',int32(AZ*360000)));
+            resp=I.query('MSS');
+            if resp~='1'
+                error('target position beyond limits')
+            end
+        end
+        
+        function ALT=get.Alt(I)
+            resp=I.query('GAC');
+            ALT=str2double(resp(1:9))/360000;
+        end
+        
+        function set.Alt(I,ALT)
+            I.query(sprintf('Sa%+09d',int32(ALT*360000)));
+            resp=I.query('MSS');
+            if resp~='1'
+                error('target position beyond limits')
+            end
+        end
+        
+        function DEC=get.Dec(I)
+            resp=I.query('GEP');
+            DEC=str2double(resp(1:9))/360000;
+        end
+        
+        function set.Dec(I,DEC)
+            I.query(sprintf('Sd%+08d',int32(DEC*360000)));
+            resp=I.query('MS1');
+            if resp~='1'
+                error('target position beyond limits')
+            end
+        end
+        
+        function RA=get.RA(I)
+            resp=I.query('GEP');
+            RA=str2double(resp(10:18))/360000;
+        end
+        
+        % East or West of pier, and counterweight positions could
+        %  be read from the last two digits of the answer to GEP.
+        %  However, they should also be understandable from Az and Alt (?)
+ 
+        function set.RA(I,RA)
+            I.query(sprintf('SRA%09d',int32(RA*360000)));
+            resp=I.query('MS1'); % choose counterweight down for now
+            if resp~='1'
+                error('target position beyond limits')
+            end
+        end
+
+    end
+    
 end
