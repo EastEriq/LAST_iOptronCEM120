ce3eeba85acca0214f9c39956e18d6c097ba62b6
Enrico Segre <enrico.segre@weizmann.ac.il>
2020-08-26 14:00:38 +0000

    check if Port available before any query

---
+++ +inst/@iOptronCEM120/private/isPortAvailable.m
@@ -0,0 +1,36 @@
+function avail=isPortAvailable(M)
+% Check if the serial or tcpip resource assigned to the mount object is
+%  still known to the system, and delete if it disappeared.
+% This is often necessary as virtual serial ports, assigned to
+%  serial-USB devices frequently disconnect and reconnect under
+%  another name, due to EMI, poor cables and whatnot.
+
+    %tic   
+
+    if isa(M.serial_resource,'tcpip')
+        % hopefully 1 ping is enough - eventually fine tune. However, only
+        %  su can ping more frequently than every 0.2sec
+        if unix(['ping -c 1 -i 0.2 -w 2 ' M.Port '>/dev/null'])
+            portlist='';
+        else
+            portlist=M.Port;
+        end
+    else
+        portlist=serialportlist; % use seriallist in rev<2019 instead
+    end
+    
+    avail=any(contains(portlist,M.Port));
+    if ~avail
+        M.report("Serial "+M.Port+' disappeared from system, closing it\n')
+        try
+            if isa(M.serial_resource,'tcpip')
+                delete(instrfind('RemoteHost',M.Port))
+            else
+                delete(instrfind('Port',Port))
+            end
+        catch
+            M.lastError=['cannot delete Port object ' M.Port ' -maybe OS disconnected it?'];
+        end
+    end
+    
+    % fprintf('availability check: %.1fms\n',toc*1000);
--- +inst/@iOptronCEM120/private/query.m
+++ +inst/@iOptronCEM120/private/query.m
@@ -1,35 +1,42 @@
-        function resp=query(I,cmd)
+        function resp=query(M,cmd)
             % Basic serial query function for iOptronCEM120
             
-            % Dispose of previous traffic potentially having
-            % filled the inbuffer, for an immediate response
-            flushinput(I.serial_resource)
-            fprintf(I.serial_resource,':%s#',cmd);
-            if strcmp(cmd,'Q')
-                pause(0.5); % abort requires a longer delay
-            elseif strcmp(cmd(1:2),'ST')
-                pause(0.7); % start and stop tracking even longer
-            else
-                pause(0.1);
-            end
-            
-            try
-                % the following must be char(fread(....)) and not the
-                %  recommended fread(....,'*char')
-                resp=char(fread(I.serial_resource,...
-                          [1,I.serial_resource.BytesAvailable],'char'));
-                % possible replies are long strings terminated by #
-                %  for get commands, or 0/1 for boolean gets, or setters
-                if isempty(resp)
-                    I.lastError='Mount did not respond. Maybe wrong command?';
-                end
-                if ~strcmp(resp(end),'#') && ...
-                        (numel(resp)==1 && ~(resp=='0' || resp=='1'))
-                    I.lastError='Response from mount incomplete';
+            % check the availability of the port
+            if ~isPortAvailable(M)
+                % ask David which kind of error reporting we expect at
+                %  abstraction level: error, F.lastError, stdout, what?
+                error(['Port ' M.Port ' disappeared'])
+            else          
+                % Dispose of previous traffic potentially having
+                % filled the inbuffer, for an immediate response
+                flushinput(M.serial_resource)
+                fprintf(M.serial_resource,':%s#',cmd);
+                if strcmp(cmd,'Q')
+                    pause(0.5); % abort requires a longer delay
+                elseif strcmp(cmd(1:2),'ST')
+                    pause(0.7); % start and stop tracking even longer
                 else
-                    I.lastError='';
+                    pause(0.1);
                 end
-            catch
-                I.lastError='cannot read from serial resource';
+                
+                try
+                    % the following must be char(fread(....)) and not the
+                    %  recommended fread(....,'*char')
+                    resp=char(fread(M.serial_resource,...
+                        [1,M.serial_resource.BytesAvailable],'char'));
+                    % possible replies are long strings terminated by #
+                    %  for get commands, or 0/1 for boolean gets, or setters
+                    if isempty(resp)
+                        M.lastError='Mount did not respond. Maybe wrong command?';
+                    end
+                    if ~strcmp(resp(end),'#') && ...
+                            (numel(resp)==1 && ~(resp=='0' || resp=='1'))
+                        M.lastError='Response from mount incomplete';
+                    else
+                        M.lastError='';
+                    end
+                catch
+                    M.lastError='cannot read from serial resource';
+                end
             end
         end
