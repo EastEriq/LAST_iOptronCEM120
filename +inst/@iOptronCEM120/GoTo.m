aae422f302a33297cb1d7e037468cdef98c48634
Enrico Segre <enrico.segre@weizmann.ac.il>
2020-08-26 14:00:12 +0000

    help of GoTo

--- +inst/@iOptronCEM120/GoTo.m
+++ +inst/@iOptronCEM120/GoTo.m
@@ -1,5 +1,9 @@
 function GoTo(I,x,y,coordtype)
 % shorthand function equivalent to setting Az,Alt or Ra,dec in a single step
+% Arguments: x:          RA or Az
+%            y:          Dec or Alt
+%            coordtype:  'eq' or 'hor' or 'azalt' (the latter two to the same effect)
+% Examples: M.GoTo(10,20,'eq')
     if ~exist('coordtype','var')
         coordtype='eq';
     end
